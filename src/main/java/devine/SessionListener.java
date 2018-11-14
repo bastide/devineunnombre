ab5c3f731cbfa95e8f461efb06a1316e445ae4a0
pedago <pedago@pcalb-mm0412.univ-jfc.fr>
2018-11-14 15:35:04 +0100

    Better javadoc commments

--- src/main/java/devine/SessionListener.java
+++ src/main/java/devine/SessionListener.java
@@ -8,29 +8,33 @@ import javax.servlet.http.HttpSessionListener;
 @WebListener("Compteur de sessions")
 public class SessionListener implements HttpSessionListener {
 
-	@Override
-	/**
-	 * Cette méthode est appelée automatiquement quand une session est créée
-	 * par @see javax.servlet.http.HttpServletRequest#getSession()
-	 */
-	public void sessionCreated(HttpSessionEvent event) {
-		ServletContext context = event.getSession().getServletContext();
-		int totalActiveSessions =
-			(Integer) context.getAttribute("numberOfPlayers");
-		totalActiveSessions++;
-		context.setAttribute("numberOfPlayers", totalActiveSessions);
-	}
+    /**
+     * Cette méthode est appelée automatiquement quand une session est créée 
+     * @see javax.servlet.http.HttpServletRequest#getSession()
+     *
+     * @param event the session event
+     */
+    @Override
+    public void sessionCreated(HttpSessionEvent event) {
+        ServletContext context = event.getSession().getServletContext();
+        int totalActiveSessions
+                = (Integer) context.getAttribute("numberOfPlayers");
+        totalActiveSessions++;
+        context.setAttribute("numberOfPlayers", totalActiveSessions);
+    }
 
-	@Override
-	/**
-	 * Cette méthode est appelée automatiquement quand une session est détruite
-	 * par @see javax.servlet.http.HttpSession#invalidate()
-	 */
-	public void sessionDestroyed(HttpSessionEvent event) {
-		ServletContext context = event.getSession().getServletContext();
-		int totalActiveSessions =
-			(Integer) context.getAttribute("numberOfPlayers");
-		totalActiveSessions--;
-		context.setAttribute("numberOfPlayers", totalActiveSessions);
-	}
+    /**
+     * Cette méthode est appelée automatiquement quand une session est détruite
+     * @see javax.servlet.http.HttpSession#invalidate()
+     *
+     * @param event the session event
+     */
+    @Override
+    public void sessionDestroyed(HttpSessionEvent event) {
+        ServletContext context = event.getSession().getServletContext();
+        int totalActiveSessions
+                = (Integer) context.getAttribute("numberOfPlayers");
+        totalActiveSessions--;
+        context.setAttribute("numberOfPlayers", totalActiveSessions);
+    }
 }
